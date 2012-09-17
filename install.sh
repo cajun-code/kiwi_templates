62edc64e0abae6982febc01f2758d152996f1686
Allan Davis <cajun.code@gmail.com>
2012-09-17 01:18:23 -0400

    Faxed install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
-export XCODE_TEMPLATE_DIR="~/Library/Developer/Xcode/Templates/File Templates"
-cp -r Kiwi $XCODE_TEMPLATE_DIR/Kiwi
+export USER_DIR=~
+export XCODE_TEMPLATE_DIR="$USER_DIR/Library/Developer/Xcode/Templates/File Templates"
+mkdir -p "$XCODE_TEMPLATE_DIR/Kiwi"
+cp -r Kiwi "$XCODE_TEMPLATE_DIR/Kiwi"
 
