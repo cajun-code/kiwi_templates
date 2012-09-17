2c4152dd555865638c281fa7d50201fa28dbdebf
Allan Davis <cajun.code@gmail.com>
2012-09-16 23:25:17 -0400

    inital import

---
+++ Kiwi/Kiwi.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,10 @@
+#import <Kiwi/Kiwi.h>
+#import "___VARIABLE_cutClass:identifier___.h"
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+    describe(@"___VARIABLE_cutClass:identifier___", ^{
+        it(@"should do somthing", ^{
+            
+        });
+    });
+SPEC_END
